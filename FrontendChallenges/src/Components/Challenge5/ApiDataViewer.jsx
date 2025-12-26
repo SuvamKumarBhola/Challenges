a30c73d7df3487fb3ecd29db6ac323692a2ae9b1
Suvam Kumar Bhola <Suvamkumarbhola2005@gmail.com>
2025-12-26 23:44:28 +0530

    Fixed some minor bugs on return statement and while fetching data in the Challenge5: ApiDataViewer

--- FrontendChallenges/src/Components/Challenge5/ApiDataViewer.jsx
+++ FrontendChallenges/src/Components/Challenge5/ApiDataViewer.jsx
@@ -5,44 +5,46 @@ function ApiDataViewer() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        const fetchData = async()=>{
-            try{
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
                 setLoading(true);
-                const response = fetch('https://jsonplaceholder.typicode.com/posts');
-                if(!response.ok){
+                const response = await fetch('https://jsonplaceholder.typicode.com/posts'); // Added await
+                if (!response.ok) {
                     throw new Error('Something went wrong');
                 }
                 const data = await response.json();
                 setPost(data);
                 setLoading(false);
                 setError(null);
-            }catch(e){
+            } catch (e) {
                 setError(e.message);
                 setPost([])
-            }finally{
+            } finally {
                 setLoading(false);
             }
         }
         fetchData();
     }, []);
 
-    if(loading) return <h1>Loading...</h1>
-    if(error) return <h1>{error}</h1>
-    if(!post) return <h1>No Data Found</h1>
-  return (
-    <div>
-        <h1>Api Data Viewer</h1>
-        <ul>
-            {post.map((post)=>{
-                <li key={post.id}>
-                    <h3>{post.title}</h3>
-                    <p>{post.body}</p>
-                </li>
-            })}
-        </ul>
-    </div>
-  )
+    if (loading) return <h1>Loading...</h1>
+    if (error) return <h1>{error}</h1>
+    if (!post) return <h1>No Data Found</h1>
+
+    return (
+        <div>
+            <h1>Api Data Viewer</h1>
+            <ul>
+                {post.map((post) => (                   
+                        <li key={post.id}>
+                            <h3>{post.title}</h3>
+                            <p>{post.body}</p>
+                        </li>
+                    )
+                )}
+            </ul>
+        </div>
+    )
 }
 
 export default ApiDataViewer
