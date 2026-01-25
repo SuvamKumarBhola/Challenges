52cdd4a3672f5b4fd1489454a8fcfb48054712b4
Suvam Kumar Bhola <Suvamkumarbhola2005@gmail.com>
2026-01-25 23:38:26 +0530

    Updated the userRoutes in BackendChallenges

--- BackendChallenges/src/routes/userRoutes.js
+++ BackendChallenges/src/routes/userRoutes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const validate = require('../middlewares/validate');
+const { createUserSchema } = require('../Validations/userSchema');
 
 const router = express.Router();
+
+router.post(
+    '/',
+    validate(createUserSchema),
+    userController.createUser 
+);
 
 router.get('/:id', userController.getUser);
 
